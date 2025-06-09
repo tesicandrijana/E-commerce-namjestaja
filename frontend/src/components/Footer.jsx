806eebbb4834397e2f225143121812e15a7b7024
dzenetamuminovic <dzeneta.muminovic2003@gmail.com>
2025-06-09 11:48:06 +0200

    promjene 2

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 import React from 'react';
 import './Footer.css';
-import { Link } from 'react-router-dom';
-
 const Footer = () => {
   return (
     <div className="main-content">
